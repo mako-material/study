ecfc41bc4d077d7bc6eead0c09be859ae35e52f2
takuroo <68760611+takuroo@users.noreply.github.com>
2020-07-25 23:55:17 +0900

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-print("hello python N vscode1")
-print("hello python N vscode2")
-print("hello python N vscode3")
-print("hello python N vscode4")
-print("hello python N vscode5")
-print("hello python N vscodeadd")
